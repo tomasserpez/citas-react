1b505f002cf72aaf119e46dc5bc4404fc980a207
Tomás Serpez <tomasserpez@gmail.com>
2022-10-20 22:31:30 -0300

    App hecha en un curso de ReactJS

---
+++ src/components/Paciente.jsx
@@ -0,0 +1,36 @@
+export const Paciente = () => {
+  return (
+    <>
+        <div className="m-3 bg-white shadow-md px-5 py-10 rounded-xl">
+            <p className="font-bold mb-3 text-grey-700 uppercase">
+                Nombre: {' '}
+                <span className="font-normal normal-case">Hook</span>
+            </p>
+
+
+            <p className="font-bold mb-3 text-grey-700 uppercase">
+                Propietario: {' '}
+                <span className="font-normal normal-case">Tomas</span>
+            </p>
+
+
+            <p className="font-bold mb-3 text-grey-700 uppercase">
+                Email: {' '}
+                <span className="font-normal normal-case">correo@correo.com</span>
+            </p>
+
+
+            <p className="font-bold mb-3 text-grey-700 uppercase">
+                Fecha Alta: {' '}
+                <span className="font-normal normal-case">10 Diciembre de 2022</span>
+            </p>
+
+
+            <p className="font-bold mb-3 text-grey-700 uppercase">
+                Síntomas: {' '}
+                <span className="font-normal normal-case">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Officiis, perferendis? Sit eveniet atque placeat sint labore illo vel tenetur necessitatibus. Minus alias expedita ex illo soluta, numquam voluptatem eligendi molestiae!</span>
+            </p>
+        </div>
+    </>
+  )
+}
